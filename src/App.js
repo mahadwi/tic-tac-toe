b8a7f5618d145d5f5b0e08b7b67cbd011259ecca
mahadwiputra008 <mahadwiputra008@gmail.com>
2023-10-31 23:49:15 +0700

    add history game

--- src/App.js
+++ src/App.js
@@ -3,45 +3,7 @@ import * as React from 'react';
 import { useState } from 'react';
 
 function Square ({value, onSquareClick}) {
-  
-
   return <button className="w-32 h-32 border text-6xl font-bold" onClick={onSquareClick}>{value}</button>
-}
-
-export default function Board() {
-  const [squares, setSquares]= useState(Array(9).fill(null));
-  const [xIsNext, setXisNext] = useState(true);
-
-  function handleClick(i){
-    if(squares[i] || calculateWinner(squares)) return;
-    const nextSquares = squares.slice();
-    nextSquares[i] = xIsNext ? 'X' : 'O';
-    setSquares(nextSquares);
-    setXisNext(!xIsNext);
-  }
-
-  const winner = calculateWinner(squares);
-  let status = ''
-  winner ? status = `Winner: ${winner}` : status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
-
-  return (
-    <>
-      <div className='flex justify-center items-center font-bold text-xl'>{status}</div>
-      <div className="flex justify-center items-center h-screen">
-        <div className='flex flex-wrap w-96 h-96'>
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-        </div>
-      </div>
-    </>
-  )
 }
 
 function calculateWinner(squares) {
@@ -64,5 +26,80 @@ function calculateWinner(squares) {
   }
 
   return false;
+}
+
+function Board({xIsNext, squares, onPlay}) {
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) return;
+
+    const nextSquares = squares.slice();
+
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status = ''
+  winner ? status = `Winner: ${winner}` : status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
+
+
+  return (
+    <>
+      <div className='font-bold text-xl'>{status}</div>
+        <div className='flex flex-wrap w-96 h-96'>
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+        </div>
+    </>
+  )
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
+  }
+
+  const moves = history.map((squares, move) => {
+    let description = '';
+
+    if(move > 0) {
+      description = `Go to move ${move}`;
+    }else {
+      description = `Go to game start`;
+    }
+
+    return (
+      <li key={move}>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-3' onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  });
+
+  return (
+    <div className='flex justify-center items-center h-screen gap-5'>
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      <ol>{moves}</ol>
+    </div>
+  )
 }
 
